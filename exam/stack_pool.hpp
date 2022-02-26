d26f210635bf45966d1e47869ebdbaf45044082e
erika.lena@outlook.it <erika.lena@outlook.it>
2022-02-26 12:07:27 +0100

    project submission

--- exam/stack_pool.hpp
+++ exam/stack_pool.hpp
@@ -69,7 +69,7 @@ class stack_pool{
   stack_type new_stack()  noexcept { stack_type new_stack{0}; return new_stack; };  // return an empty stack
 
   void reserve(size_type n) { pool.reserve(n); }; // reserve n nodes in the pool
-  size_type capacity() const noexcept { return pool.size(); }; // the capacity of the pool
+  size_type capacity() const noexcept { return pool.capacity(); }; // the capacity of the pool
 
   /* 
      check if the stack taken as input is empty by 
